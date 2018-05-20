738128703827e2421382e01e1b6fb85ce5f9f990
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2018-05-20 14:28:03 +0200

    Added author section and clear

--- bash_photo_move.sh
+++ bash_photo_move.sh
@@ -1,1 +1,11 @@
-echo This is a test
+clear
+echo '#################################'
+echo '# Author:  Ricardo van der Vlag #'
+echo '# Version: 2018-05-20           #'
+echo '# GitHub:  ricardovandervlag    #'
+echo '# Type:    Private repository   #'
+echo '#################################'
+echo
+echo This program is made to move content from a phone to a specified directory.
+echo Please enter the prefferd destination and press '[ENTER]'
+
