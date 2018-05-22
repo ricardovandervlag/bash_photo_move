d2f7f0a6e8a3e78723c5c241bd985755f0cc5f61
Ricardo van der Vlag <ricardovandervlag@gmail.com>
2018-05-22 20:17:56 +0200

    Modified source and destination check

--- bash_photo_move.sh
+++ bash_photo_move.sh
@@ -1,19 +1,30 @@
 clear
 echo '#################################'
 echo '# Author:  Ricardo van der Vlag #'
-echo '# Version: 2018-05-20           #'
+echo '# Version: 2018-05-22           #'
 echo '# GitHub:  ricardovandervlag    #'
 echo '# Type:    Private repository   #'
 echo '#################################'
 echo
 echo This program is made to move content from a phone to a specified directory.
-echo Please enter the prefferd destination and press '[ENTER]'
 
-dir="$(pwd)"
-file="/dest.txt"
-input="$dir$file"
-dest="$(cat $input)" || echo "File not found!"
-
-if [ ! -f "$dest" ]; then
-	echo "File not found"
+# Taking user input for source path
+echo "Please enter a source path and press [ENTER]"
+read source
+# Checking source path
+if [ ! -d $source ]; then
+	echo "Incorrect path"; exit 1;
+else
+	echo "Source path: $source";
 fi
+
+# Taking user input for destination path
+echo "Please enter a destination path and press [ENTER]"
+read destination
+# Checking destination path
+if [ ! -d $destination ]; then
+        echo "Incorrect path"; exit 1;
+else
+        echo "Destination path: $source";
+fi
+
