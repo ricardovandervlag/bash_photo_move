84e27bd4f4d4821486938c4ff3bd08d73b8ef2cd
Ricardo van der Vlag <ricadovandervlag@gmail.com>
2018-07-10 23:21:21 +0200

    changed echo to cp

--- bash_photo_move.sh
+++ bash_photo_move.sh
@@ -54,5 +54,5 @@ done < <(ls "$source")
 
 # Copy files
 for (( i=0; $i<($contentCount - 1); i++)); do
-	echo $source${array[$i]} $destination${array[$i]};
+	cp $source${array[$i]} $destination${array[$i]};
 done
